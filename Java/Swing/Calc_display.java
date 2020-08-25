088929a7600d9e04cb4ee8ff09815ec58ec307d8
Lazzari Carlo <carlolazzari4@gmail.com>
2020-08-25 21:42:58 +0200

    Create Calc_display.java

---
+++ Java/Swing/Calc_display.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JTextField;
+import java.awt.*;
+
+public class CalcDisplay extends JTextField {
+	public CalcDisplay(int n) {
+	super(n);
+	setFont(new Font("Bitstream Vera Sans Mono", Font.PLAIN, 30));
+	}
+}
