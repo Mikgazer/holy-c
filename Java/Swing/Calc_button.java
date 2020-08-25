c464d1eb3c3ae5ed9ec901f3ca9af4dce1b287f4
Lazzari Carlo <carlolazzari4@gmail.com>
2020-08-25 21:42:30 +0200

    Create Calc_button.java

---
+++ Java/Swing/Calc_button.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JButton;
+import java.awt.Font;
+public class CalcButton extends JButton {
+	public CalcButton(String n) {
+	super(n);
+	setFont(new Font("Bitstream Vera Sans Mono", Font.BOLD, 20));
+	}
+}
+//Creiamo ora il display della calcolatrice:
