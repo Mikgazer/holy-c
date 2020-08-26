e2e025f0a1a59fded4990a6b20254ddabacee709
Lazzari Carlo <carlolazzari4@gmail.com>
2020-08-27 00:50:58 +0200

    Create main.c

---
+++ C/list exam example/main.c
@@ -0,0 +1,100 @@
+/*Si vogliono trovare gli elementi in comune a due file testo di parole 
+(UNO.TXT e DUE.TXT), ordinarle in senso lessicografico e stamparle in un file di uscita, 
+insieme al numero totale di volte in cui compaiono nel secondo file.
+
+Il contenuto del file UNO.TXT è ordinato e le parole sono in copia unica nel file, 
+quello del file DUE.TXT non è ordinato e la stessa parola può comparire più volte nel file.
+Ogni parola sia al massimo di 30 char.*/
+
+/*
+a) Nel main, per ciascuna parola letta dal primo file,
+1. la si mostri a video
+2. poi si cerchi la parola nel secondo file, contando il totale di volte in cui compare nel
+secondo file
+
+
+
+Si svolga il punto a2) tramite la funzione funzioneA; 
+	essa riceve la parola da cercare (letta dal primo file, nel main), 
+	il puntatore al secondo file, più altri parametri a scelta, 
+	e restituisce un intero (il totale di volte in cui compare nel secondo file);
+
+b) Se il totale di volte è maggiore di 0, chiamando una seconda funzioneB, inserisca parola 
+	e totale in una lista L, ordinata sul campo parola; la funzioneB ha come parametri 
+	il puntatore alla lista e l’elemento da inserire (parola e totale) 	più altri parametri 
+	a scelta, e restituisce la nuova lista ottenuta dopo l’inserimento;
+
+c) Stampi il contenuto della lista L su un file di uscita, OUTPUT.TXT, con la stampa di parola 
+e totale suogni linea, e restituisca il numero totale di parole (la lunghezza della lista); 
+tale funzioneC ha come parametri il puntatore alla lista L, il puntatore al file 
+(più eventuali altri a scelta) e restituisce un intero che viene poi stampato dal main;
+*/
+
+#include "funzioni.h"
+int main() {
+
+	FILE *file_uno;
+	FILE *file_due;
+	element parole_uno[N];
+	element parole_due[N];
+	second_element parole_tre[N];
+
+	list l;
+	l = (list)malloc(sizeof(item));
+
+	int i = 0;
+	int k = 0;
+	int j = 0;
+	int ris = 0;
+
+	/* APERTURA SECONDO FILE (SECONDO MA VA APERTO PRIMA)*/
+	file_due = fopen("due.txt", "r");
+	if (file_due = NULL) {
+		printf("\n Apertura file due.txt non riuscita \n");
+		exit(-1);
+	}
+	while (!feof(file_due)) {
+		fscanf(file_due, "%s", parole_due[k].nome);
+		//printf("\n%s", parole_due[k].nome);
+		k++;
+	}
+	fclose(file_due);
+	/*CHIUSURA FILE*/
+
+
+	
+	/* APERTURA PRIMO FILE */
+	file_uno = fopen("uno.txt", "r");
+	if (file_uno = NULL) {
+		printf("\n Apertura file uno.txt non riuscita \n");
+		exit(-1);
+	}
+	/*FINO ALLA FINE DEL FILE*/
+	/*CONTROLLA CON LA FUNZIONE A SE E' PRESENTE ANCHE NEL SECONDO FILE(SECONDA STRUTTURA)*/
+	/*SE E' PRESENTE VIENE INSERITO NELLA LISTA, IL NUMERO DI VOLTE CHE COMPARE E' RIS*/
+	/*OVVERO LA VARIABILE TROVATO IN FUNZIONE_A*/
+	/**/
+	/**/
+	while (!feof(file_uno)) {
+		fscanf(file_uno, "%s", parole_uno[i].nome);
+		printf("\n%s", &parole_uno[i].nome);
+		ris = funzione_A(parole_uno, parole_due, i, k);
+
+		if (ris > 0) {
+			/*SE E' PRESENTE NELLA STRUTTURA 2*/
+			/*COPIA IL NOME NELLA STRUTTURA 3*/
+			/*IL NUMERO DI VOLTA CHE E' PRESENTE NELLA SECONDA STRUTTURA E' RIS*/
+			strcpy(parole_tre[j].nome, parole_uno[i].nome);
+			parole_tre[j].valore = ris;
+			j++;
+			funzione_B(parole_tre, l);
+		}
+		printf("\n La parole %s compare nel secondo file %d volte", parole_uno[i].nome, &ris);
+		i++;
+	}
+	fclose(file_uno);
+	/*CHIUSURA FILE*/
+
+	system("PAUSE");
+	return 0;
+}
