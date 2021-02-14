c3cfaeedfa8131b6e1428a1f6b1356b5f2ac9938
Mikgazer <carlolazzari4@gmail.com>
2021-02-14 16:07:43 +0100

    Update client.c

--- C/Client Server/client.c
+++ C/Client Server/client.c
@@ -18,6 +18,31 @@ void handler(int signal){
 	exit(-1);
 }
 
+/* https://man7.org/linux/man-pages/man3/getaddrinfo.3.html */
+
+
+/* 
+Given node and service, which identify an Internet host and a service, getaddrinfo() returns one or more addrinfo structures,
+       each of which contains an Internet address that can be specified in a call to bind(2) or connect(2).  
+The getaddrinfo() function combines the functionality provided by the gethostbyname(3) and getservbyname(3) functions into a single interface, 
+       - but unlike the latter functions, getaddrinfo() is reentrant and allows programs to eliminate IPv4-versus-IPv6 dependencies.
+
+The addrinfo structure used by getaddrinfo() contains the
+following fields:
+
+   struct addrinfo {
+       int              ai_flags;
+       int              ai_family;
+       int              ai_socktype;
+       int              ai_protocol;
+       socklen_t        ai_addrlen;
+       struct sockaddr *ai_addr;
+       char            *ai_canonname;
+       struct addrinfo *ai_next;
+   }; 
+*/
+
+
 int main(int argc, char **argv){
 	
 	int err, on ,cc, socket_descriptor, nread;
