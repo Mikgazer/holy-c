d872c6e826169edb22e1cec2e77a3a231c84d895
Mikgazer <carlolazzari4@gmail.com>
2021-02-18 18:08:38 +0100

    Update fizzbuzz.py

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -1,22 +1,22 @@
-def fizzbuzz(num, n, m):
-    for index in range(1, num):
-        if index % n == 0 and index % m == 0:
+def fizzbuzz(start, end, n, m):
+    for i in range(start, end):
+        if i % n == 0 and i % m == 0:
             print("Fizzbuzz")
-        elif index % n == 0:
+        elif i % n == 0:
             print("Fizz")
-        elif index % m == 0:
+        elif i % m == 0:
             print("buzz")
         else:
-            print(index)
+            print(i)
     exit(0)
 
-print("Usage: times, n, m. Type 'exit' to exit.\n")
+print("Usage: 'start, end, n, m'. Type 'exit' to exit.\n")
 while True:
     input_str = input()
     if input_str == "exit":
         exit(0)
     numbers = (input_str.split(sep=","))
-    if len(numbers) != 3:
-        print("Please insert three valid numbers.")
+    if len(numbers) != 4:
+        print("Please insert four valid numbers.")
     else:
-        fizzbuzz(int(numbers[0]), int(numbers[1]), int(numbers[2]))
+        fizzbuzz(int(numbers[0]), int(numbers[1]), int(numbers[2]), int(numbers[3]))
