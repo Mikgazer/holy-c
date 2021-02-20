ebd13530ca4662c04abbbf48f485272352ce04ea
Mikgazer <carlolazzari4@gmail.com>
2021-02-20 19:30:22 +0100

    Update element.h

--- trees/element.h
+++ trees/element.h
@@ -4,9 +4,9 @@
 typedef int element;
 typedef enum { false, true } boolean;
 
-boolean isLess(element, element); //controlla se un elemento è minore degli altri
-boolean isEqual(element, element); //uguaglianza tra gli elementi
-element getElement(void); //Input a tastiera di un elemento
-boolean isNull(element); //controlla che l'elemento sia diverso da 0 
-element sumElement(element, element); //somma di due element
-void printElement(element); //stampa a video di un elemento
+boolean isLess(element, element); /* controlla se un elemento è minore degli altri */
+boolean isEqual(element, element); /* uguaglianza tra gli elementi */
+element getElement(void); /* Input a tastiera di un elemento */
+boolean isNull(element); /* controlla che l'elemento sia diverso da 0 */
+element sumElement(element, element); /* somma di due element */
+void printElement(element); /* stampa a video di un elemento */
