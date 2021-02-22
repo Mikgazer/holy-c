b09ba05abbeab1bee07d20b10c32323c7ef7f4c0
Mikgazer <carlolazzari4@gmail.com>
2021-02-22 10:47:43 +0100

    Update intestazione.h

--- lists/intestazione.h
+++ lists/intestazione.h
@@ -5,19 +5,19 @@ typedef struct list_element {
 typedef item *list;
 
 
-//Funzioni iterative
+/* Funzioni iterative */
 #ifndef RECURSIVE
-list cons(int e, list l); //aggiunta di elementi ad una lista 
-list cons_tail(int e, list l); //aggiunta di elementi ad una lista in fondo
-//list sublist(int e, list l, list sub_l); //estrapolazione di una sotto lista
-void showList(list l); //stampa di una lista
-int member(int e, list l); //ricerca di un elemento in una lista
-int lenght(list l); //calcolo della lunghezza di una lista
-int sumlist(list l); //somma degli elementi di una lista
+list cons(int e, list l); /* aggiunta di elementi ad una lista */
+list cons_tail(int e, list l); /* aggiunta di elementi ad una lista in fondo */
+/* list sublist(int e, list l, list sub_l); estrapolazione di una sotto lista */
+void showList(list l); /* stampa di una lista */
+int member(int e, list l); /* ricerca di un elemento in una lista */
+int lenght(list l); /* calcolo della lunghezza di una lista */
+int sumlist(list l); /* somma degli elementi di una lista */
 #endif
 
 
-//Funzioni ricorsive
+/* Funzioni ricorsive */
 #ifdef RECURSIVE
 int member_r(int e, list l) 
 void showListr(list l)
